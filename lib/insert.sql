3a204aff691dfa9c786fbb812433be89d49eb081
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-20 21:44:32 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,10 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 1', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 2', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 3', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 4', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 5', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 6', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 7', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 8', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 9', 'Game', 500000, '5/1/18', '6/1/18');
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Project 10', 'Game', 500000, '5/1/18', '6/1/18');
